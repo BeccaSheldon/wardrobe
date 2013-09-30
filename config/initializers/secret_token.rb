6080d3150f99ee5ddbe9223d4d0600b2925a2b08
MakerSquare Student <students@themakersquare.com>
2013-09-30 14:45:12 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LionWitchf::Application.config.secret_key_base = '16c60b083feac91969cf4f7669d1195d43d03f9bbe66129452354c48a084e024e47cd579b90bd72aa1dd5d904b80d74030b26a9f1ebe840c38d1038bf5572fcd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LionWitchf::Application.config.session_store :cookie_store, key: '_lion-witchf_session'
